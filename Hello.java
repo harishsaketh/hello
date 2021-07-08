a088ff21c7236550af89cc18554a66a25edc2aa3
harishsaketh <50822854+harishsaketh@users.noreply.github.com>
2021-07-08 09:43:45 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello world from git code");
+        System.out.println("This is to test SCM Poll");
     }
 }
